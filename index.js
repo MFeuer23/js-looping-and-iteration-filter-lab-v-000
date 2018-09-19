7bf4edf7f7e5e8ea36a03a8749102b8619e28bbe
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-19 19:25:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
+  for (const driver of drivers) {
   drivers.filter(function (string) { return string.toLowerCase() === drivers.toLowerCase(); });
+  }
 }
