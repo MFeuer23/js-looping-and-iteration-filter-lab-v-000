3877a229f91f9c9c0192298e3ef8942bb0b5b5c4
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-19 19:50:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  drivers.filter( d (const driver of drivers) {
-    if (cb(user)) {
-    }
-  }
+  drivers.filter( d )
+  
+
 }
